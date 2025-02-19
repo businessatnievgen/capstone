cf2898ca15dc31640d4b297f7d266f3fafbcc558
businessatnievgen <business@nievgen.com>
2025-02-19 16:12:41 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("THIS IS THE GREATEST SHOW")
+st.subheader("It is always fun to be here!",":100:")
+st.write("Join us and earn great salary!")
+st.divider()
+st.image("IMG_01.jpg","Kunyari masaya pero mas masaya")
+
+
